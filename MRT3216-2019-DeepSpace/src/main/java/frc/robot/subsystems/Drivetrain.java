d393c8f7970a9a31b36577c85d86577803b9e729
Braeden Hunt <20hunbra@student.mcpsmt.org>
2018-11-08 08:29:32 -0700

    Copied Over Drivetrain
    Commented out all robot specific code along with logging info

---
+++ MRT3216-2019-DeepSpace/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,92 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Drivetrain extends Subsystem {
+	//DRIVETRAIN SUBSYSTEM COPIED OVER FROM 2018. ALL ROBOT SPECIFIC CODE IS COMMENTED OUT
+	//LOGGING INFORMATION IS ALSO COMMENTED OUT
+
+	/** Configuration Constants ***********************************************/
+	//private static final Logger.Level LOG_LEVEL = RobotMap.LOG_DRIVETRAIN;
+
+	/** Instance Variables ****************************************************/
+	private SpeedController leftMotors, rightMotors;
+	//private Logger log = new Logger(LOG_LEVEL, getName());
+	double leftPowerOld, rightPowerOld;
+	Timer timer = new Timer();
+
+	public Drivetrain() {
+		//log.add("Constructor", LOG_LEVEL);
+		/*
+		if (RobotMap.currentBot == RobotMap.Bot.MAINBOT || RobotMap.currentBot == RobotMap.Bot.TESTBOARD) {
+			leftMotors = new VictorSP(RobotMap.PWM_LEFT_MOTOR);
+			rightMotors = new VictorSP(RobotMap.PWM_RIGHT_MOTOR);
+
+			initMotor((VictorSP) leftMotors, RobotMap.REVERSE_LEFT_MOTOR);
+			initMotor((VictorSP) rightMotors, RobotMap.REVERSE_RIGHT_MOTOR);
+		} else {
+			leftMotors = new Talon(RobotMap.PWM_LEFT_MOTOR);
+			rightMotors = new Talon(RobotMap.PWM_RIGHT_MOTOR);
+
+			initMotor((Talon) leftMotors, RobotMap.REVERSE_LEFT_MOTOR);
+			initMotor((Talon) rightMotors, RobotMap.REVERSE_RIGHT_MOTOR);
+		}
+		*/
+	}
+
+	private void initMotor(Talon motor, boolean reverse) {
+		motor.setInverted(reverse); // affects percent Vbus mode???
+	}
+
+	private void initMotor(VictorSP motor, boolean reverse) {
+		motor.setInverted(reverse);
+	}
+
+	@Override
+	public void initDefaultCommand() {
+		/*
+		if (RobotMap.DriveMode.TANK == RobotMap.currentDriveMode) {
+			setDefaultCommand(new Drivetrain_TankDrive());
+		} else {
+			setDefaultCommand(new Drivetrain_ArcadeDrive());
+		}
+		*/
+	}
+
+	/** Methods for setting the motors *************************************/
+	public void setPower(double leftPower, double rightPower) {
+		this.setPower(leftPower, rightPower, false);
+	}
+
+	public void setPower(double leftPower, double rightPower, boolean driveStraight) {
+		leftPower = safetyCheck(leftPower);
+		rightPower = safetyCheck(rightPower);
+
+		leftMotors.set(leftPower);
+		rightMotors.set(rightPower);
+
+		leftPowerOld = leftPower;
+		rightPowerOld = rightPower;
+	}
+
+	public void driveStraight(double power) {
+
+	}
+
+	public void stop() {
+		setPower(0.0, 0.0);
+	}
+
+	private double safetyCheck(double power) {
+		power = Math.min(1.0, power);
+		power = Math.max(-1.0, power);
+		return power;
+	}
+}
