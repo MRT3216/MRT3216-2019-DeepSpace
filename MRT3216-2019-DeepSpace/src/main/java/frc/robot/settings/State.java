91467e337866f0a0f6751f65fb6249e81e6474b0
Braeden Hunt <20hunbra@student.mcpsmt.org>
2019-01-07 09:15:38 -0700

    Added State class and Outlined Elevator goto()

---
+++ MRT3216-2019-DeepSpace/src/main/java/frc/robot/settings/State.java
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.settings;
+
+/**
+ * Add your docs here.
+ */
+public class State {
+
+    public static enum ELEVATOR_POS {
+        BOTTOM, MIDDLE, TOP
+    }
+
+    public static enum GAME_PIECE {
+        CARGO, PANEL
+    }
+}
--- MRT3216-2019-DeepSpace/src/main/java/frc/robot/subsystems/Elevator.java
+++ MRT3216-2019-DeepSpace/src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,7 +3,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-import frc.robot.settings.*;
+import frc.robot.settings.RobotMap;
+import frc.robot.settings.State;
+import frc.robot.setting.Constants;
 
 /**
  *
@@ -23,6 +25,37 @@ public class Elevator extends Subsystem {
 		motor.set(power);
 	}
 
+	
+	//code should get current position of elevator and then decide which direction to go until correct limit switch is hit.
+
+	public void goto(ELEVATOR_POS pos, GAME_PIECE piece) {
+		if(piece == State.GAME_PIECE.CARGO) {
+			if(pos == State.ELEVATOR_POS.TOP) { 		//GOTO top slot for cargo
+
+			}
+			else if(pos == State.ELEVATOR_POS.MIDDLE) { //GOTO middle slot for cargo
+
+			}
+			else { 										//GOTO bottom slot for cargo
+
+			}
+		}
+		else {
+			if(pos == State.ELEVATOR_POS.TOP) { 		//GOTO top slot for panel
+
+			}
+			else if(pos == State.ELEVATOR_POS.MIDDLE) { //GOTO middle slot for panel
+
+			}
+			else { 										//GOTO bottom slot for panel
+
+			}
+		}
+
+	}
+
+
+
 	public void stop() {
 		setPower(0.0);
 	}
@@ -36,6 +69,8 @@ public class Elevator extends Subsystem {
 		 */
 		return power;
 	}
+
+
 
 	private double heightCheck(double power) {
 		return power; //TODO: implement top and bottom limit switches and check to make sure elevator doesn't move past them.
